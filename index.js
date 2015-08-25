f219c1d1dbd0cdf76573c5c115ed80e5678b9ecc
Jacob Angel <jacob.angel@gmail.com>
2015-08-25 00:49:28 -0400

    Fib is a good starting point.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('babel/polyfill');
 const gen = require('./src/generators');
 
 gen.run();
--- src/generators.js
+++ src/generators.js
@@ -1,16 +1,19 @@
 function* fib() {
-  let n = 1;
-  let arr = [0, 1];
-  while (n++) {
-    arr[n] = arr[n - 1] + arr[n - 2];
+  let n = 0;
+  const arr = [0, 1];
+  while (++n) {
+    if (n > 1) {
+      arr[n] = arr[n - 1] + arr[n - 2];
+    }
     yield arr[n];
   }
 }
 
 module.exports = {
   run() {
-    for (let i = 0, im = 10; i < im; i++) {
-      console.log(fib.next().value);
+    let fibGenerator = fib();
+    for (let i = 0, im = 30; i < im; i++) {
+      console.log(fibGenerator.next().value);
     }
   },
 };
